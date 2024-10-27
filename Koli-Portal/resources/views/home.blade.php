13b82d47dc0ce7b148c96a0eb9ee264042f7d20d
K-Georgei <91503291+K-Georgei@users.noreply.github.com>
2024-10-27 13:05:55 +0100

    base laravel project

---
+++ Koli-Portal/resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zirzen Janka Kollégium Portál</title>
+</head>
+<body>
+    <h1>Home</h1>
+</body>
+</html>
